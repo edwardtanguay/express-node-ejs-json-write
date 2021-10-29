1a80372aea81ffe7994c0c409630eb43ae470775
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-10-29 14:13:06 +0200

    finished

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,14 +5,21 @@ const router = express.Router();
 router.use(express.json());
 
 router.patch('/changeState', (req, res) => {
-	const { showImages } = req.body;
-	console.log(req.body);
-	qfil.getJsonDataFromFile('siteData.json', (siteData) => {
-		siteData.showImages = showImages;
-		qfil.saveJsonDataToFile('siteData.json', siteData, () => {
-			res.status(200);
+	const { showImages, password } = req.body;
+	if (password === 'pass123') {
+		qfil.getJsonDataFromFile('siteData.json', (siteData) => {
+			siteData.showImages = showImages;
+			qfil.saveJsonDataToFile('siteData.json', siteData, () => {
+				res.status(200).json({
+					message: showImages ? `This site now shows images.` : `This site now hides images.`
+				});
+			});
 		});
-	});
+	} else {
+		res.status(401).json({
+			message: `Password was not correct. No settings were changed.`
+		});
+	}
 });
 
 export default router;
