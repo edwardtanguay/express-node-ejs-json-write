848880dbad6d3d7f81b5ab4c03c506bb4728565e
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-06 21:05:07 +0100

    changed port

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import * as qfil from './qtools/qfil.js';
 import apiRouter from './routes/api.js';
 
 const app = express();
-const port = process.env.PORT || 3011;
+const port = process.env.PORT || 3012;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './public/views'));
